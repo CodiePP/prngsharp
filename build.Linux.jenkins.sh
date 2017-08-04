6223c2c9f7d2c4877e97f41709ceb4a3e436e2d2
Alexander V. Diemand <codieplusplus@apax.net>
2017-08-04 15:38:59 +0200

    update version; jenkins job

---
+++ build.Linux.jenkins.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+PLATFRM="Any CPU"
+
+sh mk_linux.sh
+
+sn -k sharpPRNG/sharpPRNG.snk
+
+xbuild /t:clean /p:Configuration="Debug" /p:Platform=${PLATFRM} sharpPRNG/sharpPRNG.sln
+xbuild /p:Configuration="Debug" /p:Platform=${PLATFRM} sharpPRNG/sharpPRNG.sln
+
+cp libprngCpp.1.0.4.so  sharpPRNG/testSharpPRNG/bin/Debug/
+
+mono sharpPRNG/testSharpPRNG/bin/Debug/testSharpPRNG.exe
+
