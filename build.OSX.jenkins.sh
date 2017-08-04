ee01cfe6798a696eca330816f44ee53bced58437
Alexander V. Diemand <codieplusplus@apax.net>
2017-08-05 00:10:35 +0200

    osx jenkins

---
+++ build.OSX.jenkins.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+PLATFRM="Any CPU"
+
+sh mk_osx.sh
+
+sn -k sharpPRNG/sharpPRNG.snk
+
+xbuild /t:clean /p:Configuration="Debug" /p:Platform="${PLATFRM}" sharpPRNG/sharpPRNG.sln
+xbuild /p:Configuration="Debug" /p:Platform="${PLATFRM}" sharpPRNG/sharpPRNG.sln
+
+cp libprngCpp.1.0.4.dylib  sharpPRNG/testSharpPRNG/bin/Debug/
+
+mono sharpPRNG/testSharpPRNG/bin/Debug/testSharpPRNG.exe
+
