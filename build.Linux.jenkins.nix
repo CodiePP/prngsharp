33e80454a6ab3ecc92281e306bcd2bbfb71ce9bc
Alexander Diemand <codieplusplus@apax.net>
2020-05-02 12:20:39 +0200

    nix support
    Signed-off-by: Alexander Diemand <codieplusplus@apax.net>

---
+++ build.Linux.jenkins.nix
@@ -0,0 +1,18 @@
+#!/usr/bin/env nix-shell
+#! nix-shell -i bash -p gcc mono zlib openssl
+
+set -e
+
+PLATFRM="Any CPU"
+
+sh mk_linux.sh
+
+sn -k sharpPRNG/sharpPRNG.snk
+
+xbuild /t:clean /p:Configuration="Debug" /p:Platform="${PLATFRM}" sharpPRNG/sharpPRNG.sln
+xbuild /p:Configuration="Debug" /p:Platform="${PLATFRM}" sharpPRNG/sharpPRNG.sln
+
+cp libprngCpp.linux.1.0.5.so  sharpPRNG/testSharpPRNG/bin/Debug/
+
+mono sharpPRNG/testSharpPRNG/bin/Debug/testSharpPRNG.exe
+
